e0a89a497826e27a0027b5751454fd5b754fe87f
Ari Strandburg-Peshkin <astrandburg@ab.mpg.de>
2024-08-25 10:58:16 +0200

    found yet another axytrek format to incorporate

--- R/import_axytrek_gps_file.R
+++ R/import_axytrek_gps_file.R
@@ -21,22 +21,31 @@ import_axytrek_gps_file <- function(input_file_path){
 
   file_lines <- readLines(input_file_path)
   n_tabs <- stringr::str_count(file_lines, pattern = '\t')
-  lines_to_use <- which(n_tabs %in% c(7,8))
+  lines_to_use <- which(n_tabs %in% c(7,8,13))
   file_lines_use <- file_lines[lines_to_use]
   file_lines_split <- sapply(file_lines_use, FUN = function(x){return(strsplit(x, '\t'))})
   df <- do.call(rbind.data.frame, file_lines_split)
 
-  #combine first 2 columns into full timestamp
-  if(ncol(df)==9){
+  #combine first 2 columns into full timestamp if needed
+  if(ncol(df)>=9){
     df[,1] <- paste(df[,1],df[,2],sep=',')
     df <- df[,-2]
   }
 
   #rename columns
-  colnames(df) <- c('timestamp','location.lat','location.long','V4','V5','satellite.count','V7','V8')
+  if(ncol(df) == 8){
+    colnames(df) <- c('timestamp','location.lat','location.long','V4','V5','satellite.count','V7','V8')
+  }
+  if(ncol(df) == 13){
+    colnames(df) <- c('timestamp','location.long','location.lat','V4','V5','V6','V7','satellite.count','V9','V10','V11','V12','V13')
+  }
 
   #parse timestamp
-  df$timestamp <- lubridate::parse_date_time(df$timestamp, tz = 'UTC', orders = c('%d/%m/%Y,%H:%M:%S', '%Y-%m-%d,%H:%M:%S'))
+  df$timestamp <- lubridate::parse_date_time(df$timestamp, tz = 'UTC', orders = c('%d/%m/%Y,%H:%M:%S',
+                                                                                  '%Y-%m-%d,%H:%M:%S'))
+
+  #return only relevant columns
+  df <- df[,c('timestamp','location.lat','location.long','satellite.count')]
 
   #return data frame
   return(df)
