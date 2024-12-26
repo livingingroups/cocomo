335e5f48834586fc69b932d4c9dd4933d0511b84
Brock <kbrock@ab.mpg.de>
2024-12-26 12:48:58 +0100

    fix typo

--- R/get_angle_between_vectors.R
+++ R/get_angle_between_vectors.R
@@ -4,7 +4,7 @@
 #'Vector 1 is defined as the vector pointing from the point `(x1_i, y1_i)` to `(x1_f, y1_f)` and
 #'vector 2 is defined as the vector point from the point `(x2_i, y2_i)` to `(x2_f, y2_f)`.
 #'The angle is defined as the angle produced if the two vectors are joined at the initial
-#'endpoints (rather than head-to-tail). The reuslting angle is always positive.
+#'endpoints (rather than head-to-tail). The resulting angle is always positive.
 #'
 #' @author Ariana Strandburg-Peshkin
 #' @author NOT YET CODE REVIEWED
