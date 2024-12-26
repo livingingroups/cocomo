f9dd40695b2b6fd95fd510e8d337e68e75803415
Brock <kbrock@ab.mpg.de>
2024-12-26 12:47:02 +0100

    fixup

--- R/get_group_heading_and_speed.R
+++ R/get_group_heading_and_speed.R
@@ -4,7 +4,7 @@
 #' going either forward (into the future) or backward (into the past) when computing the heading
 #'
 #' @author Ariana Strandburg-Peshkin (primary author)
-#' @author NOT YET CODE REVIEWED
+#' @author Reviewed by Brock
 #'
 #' @param xs `N x n_times` matrix giving x coordinates of each individual over time
 #' @param ys `N x n_times` matrix giving y coordinates of each individual over time
