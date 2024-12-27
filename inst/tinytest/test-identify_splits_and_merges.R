6d4d9303ac3eb46ab6529c0c59f75a978c3823f3
Brock <kbrock@ab.mpg.de>
2024-12-27 10:29:16 +0100

    add shuffle

--- inst/tinytest/test-identify_splits_and_merges.R
+++ inst/tinytest/test-identify_splits_and_merges.R
@@ -1,7 +1,7 @@
-# TODO: add shuffle, test boundaries, test more complex entangle/disentangle
+# TODO: test boundaries, test more complex entangle/disentangle
 # multiple fisions/fusions at single timemstamp
 
-n_times = 9
+n_times = 10
 N = 4
 
 loc_lookup <- list(
@@ -12,10 +12,10 @@ loc_lookup <- list(
 )
 
 loc = c(
-  'a', 'a', 'a', 'b', 'b', 'd', 'd', 'd', 'd',
-  'a', 'a', 'a', 'b', 'b', 'd', 'd', 'd', 'd',
-  'a', 'a', 'a', 'c', 'c', 'd', 'd', 'd', 'd',
-  'a', 'a', 'a', 'c', 'c', 'd', 'd', 'd', 'd'
+  'a', 'a', 'a', 'b', 'b', 'b', 'd', 'd', 'd', 'd', 
+  'a', 'a', 'a', 'b', 'b', 'c', 'd', 'd', 'd', 'd',
+  'a', 'a', 'a', 'c', 'c', 'b', 'd', 'd', 'd', 'd',
+  'a', 'a', 'a', 'c', 'c', 'c', 'd', 'd', 'd', 'd'
 )
 
 xs <- matrix(
@@ -44,26 +44,26 @@ s_and_m <- identify_splits_and_merges(xs,ys,as.POSIXct(1:n_times),.5,.5, names =
 
 expect_equal(
   data.frame(
-    event_idx = 1:2,
-    tidx = c(3,5),
-    event_type = c('fission', 'fusion'),
-    n_groups_before = c(1,2),
-    n_groups_after = c(2,1),
-    big_group_idxs = I(list(1:4, 1:4)),
-    big_group = I(list(ind_names, ind_names)),
-    group_A_idxs = I(list(1:2, 1:2)),
-    group_B_idxs = I(list(3:4, 3:4)),
-    group_A = I(list(c('Lily', 'Pan'), c('Lily', 'Pan'))),
-    group_B = I(list(c('Tamu', 'Winnie'), c('Tamu', 'Winnie'))),
-    n_A = c(2,2),
-    n_B = c(2,2),
-    n_big_group = c(4,4)
+    event_idx = 1:3,
+    tidx = c(3,5,6),
+    event_type = c('fission','shuffle','fusion'),
+    n_groups_before = c(1,2,2),
+    n_groups_after = c(2,2,1),
+    big_group_idxs = I(list(1:4, NA, 1:4)),
+    big_group = I(list(ind_names,NA,ind_names)),
+    group_A_idxs = I(list(1:2,NA,c(1,3))),
+    group_B_idxs = I(list(3:4,NA,c(2,4))),
+    group_A = I(list(c('Lily', 'Pan'), NA, c('Lily', 'Tamu'))),
+    group_B = I(list(c('Tamu', 'Winnie'), NA, c('Pan', 'Winnie'))),
+    n_A = c(2,NA,2),
+    n_B = c(2,NA,2),
+    n_big_group = c(4,NA,4)
   ),
   s_and_m$events_detected,
   check.attributes = FALSE
 )
 
-tinytest::expect_equal(
+expect_equal(
   list(
     list(
       t = 3,
@@ -75,7 +75,15 @@ tinytest::expect_equal(
     ),
     list(
       t = 5,
-      groups_before = list(list(1:2), list(3:4)), #list( A = c('Lily', 'Pan'), B = c('Tamu', 'Winnie')),
+      groups_before = list(list(1:2), list(3:4)),
+      groups_after = list(list(c(1,3)), list(c(2,4))),
+      event_type = 'shuffle',
+      n_groups_before = 2,
+      n_groups_after = 2
+    ),
+    list(
+      t = 6,
+      groups_before = list(list(c(1,3)), list(c(2,4))),
       groups_after = list(list(1:4)), # list(A = ind_names),
       event_type = 'fusion',
       n_groups_before = 2,
