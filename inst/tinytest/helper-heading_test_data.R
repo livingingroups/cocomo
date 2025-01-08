abcbc62561502fa35d9252db69e2619a09da9775
Brock <kbrock@ab.mpg.de>
2025-01-08 13:05:07 +0100

    Refactor heading and speed tests

--- inst/tinytest/helper-heading_test_data.R
+++ inst/tinytest/helper-heading_test_data.R
@@ -13,7 +13,7 @@ shift <- function(v, n=1) return(if (n>0) c(
   )
 )
 
-## Helper functions ----
+## Test Helper functions ----
 expect_equal(shift(1:10, 2), c(9:10,1:8))
 expect_equal(shift(1:10, -2), c(3:10,1:2))
 
@@ -47,9 +47,11 @@ y       <-      c(  0,   1,   2,   1,   2,   3,   3,   3,   3,   2,   1,   2,   
 
 len <- length(x)
 
-### Expected output (speed, headings, dt)   ----
+### Expected output (speed, headings, dt) ----
 
-# Temportal, t_window = 1
+#### Temporal ----
+
+# Temporal, t_window = 1
 speed_1 <- sqrt(c(  2,   2,   2,   2,   2,   1,   1,   1,   2,   2,   1,   1,   1,   2,   2,   2,   2,   1,   1,   0,   2,   2,   1,   1,   1,   1,   1,   1,   1,   1,  NA))
 h_1     <-   pi*c(1/4, 1/4,-1/4, 1/4, 1/4,   0,   0,   0,-1/4,-1/4, 1/2, 1/2, 1/2, 3/4, 3/4, 3/4, 3/4,   1,   1, NaN,-1/4,-1/4, 1/2, 1/2, 1/2, 1/2,   0,   0,   0,   0,  NA)
 
@@ -57,29 +59,104 @@ h_1     <-   pi*c(1/4, 1/4,-1/4, 1/4, 1/4,   0,   0,   0,-1/4,-1/4, 1/2, 1/2, 1/
 speed_2 <- sqrt(c(  8,   4,   4,   8,   5,   4,   4,   5,   8,   1,   4,   4,   5,   8,   8,   8,   5,   4,   1,   2,   8,   1,   4,   4,   4,   2,   4,   4,   4,  NA,  NA))/2
 h_2     <-   pi*c(1/4,   0,   0, 1/4,  c1,   0,   0,-c1, -1/4,   0, 1/2, 1/2,  c2, 3/4, 3/4, 3/4,  c3,   1,   1,-1/4,-1/4,   0, 1/2, 1/2, 1/2, 1/4,   0,   0,   0,   NA, NA)
 
+#### Spatial ----
+
+# constants
+all_NA <- rep(NA_real_, len)
 
 # Spatial, R = 1, forward = TRUE
-# dt varies, then speed and heading can be looked up from above based on dt.
+# dt varies
 dt_R1f  <-      c(  1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   2,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,  NA)
+# look up speed and headings  from temporal case based on dt
+speed_R1f <- ifelse(
+  dt_R1f == 1,
+  speed_1,
+  ifelse(
+    dt_R1f == 2,
+    speed_2,
+    NA_integer_
+  )
+)
+
+h_R1f <- ifelse(
+  dt_R1f == 1,
+  h_1,
+  ifelse(
+    dt_R1f == 2,
+    h_2,
+    NA_integer_
+  )
+)
 
 # Spatial, R = 1, forward = FALSE
-dt_R1b  <-      c( NA,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   2,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1)
+dt_R1b <- shift(dt_R1f)
+
+speed_R1b <- ifelse(
+  dt_R1b == 1,
+  shift(speed_1),
+  ifelse(
+    dt_R1b == 2,
+    shift(speed_2, 2),
+    NA_integer_
+  )
+)
+
+h_R1b <- ifelse(
+  dt_R1b == 1,
+  shift(h_1),
+  ifelse(
+    dt_R1b == 2,
+    shift(h_2, 2),
+      NA_integer_
+  )
+)
 
 # Spatial, R = 1.5, forward = TRUE
 dt_R1_5f<-      c(  2,   2,   2,   2,   2,   2,   2,   2,   2,   4,   2,   2,   2,   2,   2,   2,   2,   2,   4,   3,   2,   4,   2,   2,   2,   3,   2,   2,   2,  NA,  NA)
 
+# for R = 1.5, most of dt are 2, so we can just pull those from  speed_2, heading_2
 speed_R1_5f <- integer(len)
 speed_R1_5f[-c(10,19,20,22,26)] <- speed_2[-c(10,19,20,22,26)]
+
+h_R1_5f <- integer(len)
+h_R1_5f[-c(10,19,20,22,26)] <- h_2[-c(10,19,20,22,26)]
+
+# for at those indices where  dt is not 2, set speed and heading manually
+
 speed_R1_5f[10] <- sqrt(5)/4
 speed_R1_5f[19] <- sqrt(5)/4
 speed_R1_5f[20] <- 2*sqrt(2)/3
 speed_R1_5f[22] <- sqrt(5)/4
 speed_R1_5f[26] <- sqrt(5)/3
 
-h_R1_5f <- integer(len)
-h_R1_5f[-c(10,19,20,22,26)] <- h_2[-c(10,19,20,22,26)]
 h_R1_5f[10] <- atan2(2,1)
 h_R1_5f[19] <- -atan2(2,1)
 h_R1_5f[20] <- -pi/4
 h_R1_5f[22] <- atan2(2,1)
 h_R1_5f[26] <- atan2(1,2)
+
+# Spatial, R = 1, t_indxs = c(5, 20)
+
+t_idxs <- c(5, 20)
+is_selected <- seq_len(len) %in% t_idxs
+
+dt_t_idxs_set <- ifelse(
+  is_selected,
+  # hardcoded values for indexes 5, 20
+  c(1, 2),
+  NA_integer_
+)
+
+speed_t_idxs_set <- ifelse(
+  is_selected,
+  # hardcoded values for indexes 5, 20
+  sqrt(c(2,.5)),
+  NA_integer_
+)
+
+h_t_idxs_set <- ifelse(
+  is_selected,
+  # hardcoded values for indexes 5, 20
+  pi*c(1/4, -1/4),
+  NA_integer_
+)
--- inst/tinytest/test-get_heading_and_speed_spatial.R
+++ inst/tinytest/test-get_heading_and_speed_spatial.R
@@ -1,54 +1,48 @@
 source('helper-heading_test_data.R')
 
-all_NA <- rep(NA_real_, len)
-
 ## R = 1, forward
 h_s_R1f <- get_heading_and_speed_spatial(x, y, 1)
 
-# check that i calculates dt correctly
-expect_equal(h_s_R1f$dts, dt_R1f)
-
-
-# check heads given dt
-expect_equal(all_NA[is.na(dt_R1f)], h_s_R1f$heads[is.na(dt_R1f)])
-expect_equal(h_1[dt_R1f == 1], h_s_R1f$heads[dt_R1f == 1])
-expect_equal(h_2[dt_R1f == 2], h_s_R1f$heads[dt_R1f == 2])
-
-# check speeds given dt
-expect_equal(all_NA[is.na(dt_R1f)], h_s_R1f$speeds[is.na(dt_R1f)])
-expect_equal(speed_1[dt_R1f == 1], h_s_R1f$speeds[dt_R1f == 1])
-expect_equal(speed_2[dt_R1f == 2], h_s_R1f$speeds[dt_R1f == 2])
-
-h_s_R1b <- get_heading_and_speed_spatial(x, y, 1, forward=FALSE)
+expect_equal(
+  get_heading_and_speed_spatial(x, y, 1),
+  list(
+    heads = h_R1f,
+    speeds = speed_R1f,
+    dts = dt_R1f
+  ),
+  info = "Checks dt, heading, speed with R=1, forward = TRUE"
+)
 
 ## R = 1, backward
-# check that i calculates dt correctly
-expect_equal(h_s_R1b$dts, dt_R1b)
+expect_equal(
+  get_heading_and_speed_spatial(x, y, 1, forward=FALSE),
+  list(
+    heads = h_R1b,
+    speeds = speed_R1b,
+    dts = dt_R1b
+  ),
+  info = "Checks dt, heading, speed with R=1, forward = FALSE"
+)
 
-# check heads given dt
-expect_equal(all_NA[is.na(dt_R1b)], h_s_R1b$heads[is.na(dt_R1b)])
-expect_equal(shift(h_1)[dt_R1b == 1], h_s_R1b$heads[dt_R1b == 1])
-expect_equal(shift(h_2, 2)[dt_R1b == 2], h_s_R1b$heads[dt_R1b == 2])
+# R = 1.5
+expect_equal(
+  get_heading_and_speed_spatial(x, y, 1.5),
+  list(
+    heads = h_R1_5f,
+    speeds = speed_R1_5f,
+    dts = dt_R1_5f
+  ),
+  info = "Checks dt, heading, speed with R=1.5, forward = TRUE"
+)
 
-# check speeds given dt
-expect_equal(all_NA[is.na(dt_R1b)], h_s_R1b$speeds[is.na(dt_R1b)])
-expect_equal(shift(speed_1)[dt_R1b == 1], h_s_R1b$speeds[dt_R1b == 1])
-expect_equal(shift(speed_2, 2)[dt_R1b == 2], h_s_R1b$speeds[dt_R1b == 2])
+# t_idxs is set
 
-
-## set t_ index
-h_s_gappy <- get_heading_and_speed_spatial(x, y, 1, c(5, 20))
-na_idx = c(1:4, 6:19, 21:len)
-
-expect_equal(all_NA[na_idx], h_s_gappy$heads[na_idx])
-expect_equal(all_NA[na_idx], h_s_gappy$speeds[na_idx])
-expect_equal(all_NA[na_idx], h_s_gappy$dts[na_idx])
-
-expect_equal(sqrt(c(2,.5)), h_s_gappy$speeds[c(5,20)])
-expect_equal(c(1, 2), h_s_gappy$dts[c(5,20)])
-expect_equal(pi*c(1/4, -1/4), h_s_gappy$heads[c(5,20)])
-
-h_s_R1_5f <- get_heading_and_speed_spatial(x, y, 1.5)
-expect_equal(dt_R1_5f, h_s_R1_5f$dts)
-expect_equal(speed_R1_5f, h_s_R1_5f$speeds)
-expect_equal(h_R1_5f, h_s_R1_5f$heads)
+expect_equal(
+  get_heading_and_speed_spatial(x, y, 1, t_idxs),
+  list(
+    heads = h_t_idxs_set,
+    speeds = speed_t_idxs_set,
+    dts = dt_t_idxs_set
+  ),
+  info = "Checks dt, heading, speed with R=1, t_idxs set"
+)
