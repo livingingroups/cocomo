ce618b3c6bb4c30c29bb7ec94371f4819135a17a
Ariana Strandburg-Peshkin <ari@kn-eas-m019.local>
2025-02-04 09:44:38 +0100

    updated get_speed to change time_res to seconds_per_timestep for consistency with naming in other functions

--- R/get_speed.R
+++ R/get_speed.R
@@ -2,23 +2,23 @@
 #'
 #' Gets an individual's speed over time, using a specified time step to compute the speed
 #'
-#'The speed is computed as `speeds[t] = sqrt ( (x_i[t+t_window] - x_i[t] )^2 + ( y_i[t+t_window] - y_i[t] )^2 ) / (time_res * t_window)`
+#'The speed is computed as `speeds[t] = sqrt ( (x_i[t+t_window] - x_i[t] )^2 + ( y_i[t+t_window] - y_i[t] )^2 ) / (seconds_per_timestep * t_window)`
 #'Note that by default `speeds[t]` gives the instantaneous speed between time `t` and time `t + t_window`, i.e. the speeds are computed
 #'based on the future not on the past. If `forward` is set to `F`, the speeds will be computed based on the past, as:
-#'`speeds[t] = sqrt ( (x_i[t] - x_i[t-t_window] )^2 + ( y_i[t] - y_i[t-t_window] )^2 ) / (time_res * t_window)`
+#'`speeds[t] = sqrt ( (x_i[t] - x_i[t-t_window] )^2 + ( y_i[t] - y_i[t-t_window] )^2 ) / (seconds_per_timestep * t_window)`
 #'
 #' @author Ariana Strandburg-Peshkin
 #' @author Reviewed by Brock
 #'
 #' @param	x_i x coordinates of the individual (a vector whose length is the number of timesteps) or of a group centroid
 #' @param y_i y coordinates of the individual (a vector whose length is the number of timesteps) or of the group centroid
-#' @param time_res the number of seconds between timestamps (e.g. for data recorded at 1 Hz, the value would be 1, for data recorded every 30 sec, the value would be 30)
+#' @param seconds_per_timestep the number of seconds between timestamps (e.g. for data recorded at 1 Hz, the value would be 1, for data recorded every 30 sec, the value would be 30)
 #' @param t_window the time window over which to compute speed (number of time steps into the future or past)
 #' @param forward a boolean value (defaults to T) indicating whether to compute speeds forward in time (if T) or backward in time (if F)
 #' @return Returns `speeds`: a time series of the speed of individual (a vector of the same length as x_i and y_i)
 #'
 #' @export
-get_speed <- function(x_i, y_i, time_res, t_window = 1, forward = T){
+get_speed <- function(x_i, y_i, seconds_per_timestep, t_window = 1, forward = T){
 
   #check that x_i and y_i are the same length
   if(length(x_i) != length(y_i)){
@@ -49,7 +49,7 @@ get_speed <- function(x_i, y_i, time_res, t_window = 1, forward = T){
   dy <- y2 - y1
 
   #get change in time for each step
-  dt <- time_res * t_window
+  dt <- seconds_per_timestep * t_window
 
   #get instantaneous speed
   speeds <- sqrt( dx^2 + dy^2 ) / dt
