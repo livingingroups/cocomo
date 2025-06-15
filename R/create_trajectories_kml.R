3aeb79a5eae9d5c18517cc7dca9a6f3309ebea43
Ariana Strandburg-Peshkin <ari@kn-eas-m019.local>
2025-06-15 15:18:29 +0200

    added elongation code

--- R/create_trajectories_kml.R
+++ R/create_trajectories_kml.R
@@ -16,6 +16,8 @@
 #' @param cols vector of length `N` giving colors for each individual, e.g. 'ffed8031' (first two elements give transparency, last 6 are color specified in hex). If NULL, trajectories will be white.
 #' @param icons vector of length `N` specifying icons (further information below)
 #' @param fixed_locs data frame with lon and lat coordinates of fixed locations to label (e.g. dens) - must have columns 'names','lon','lat'
+#' @param calls data frame with columns `ind_idx`,`time_idx`,`call_type`, and `time`
+#' @param calls_icons vector of length equal to the number of call types specifying which icons will be used for displaying calls
 #'
 #' @section Additional details on icon and line color specification:
 #' You can specify icons by giving a vector of filenames (character strings) pointing to images on your computer (e.g. png works).
@@ -29,7 +31,7 @@
 #' @returns Creates and saves a kml to the specified output_file_path which can be loaded into Google Earth to view animated trajectories
 #' @export
 #'
-create_trajectories_kml <- function(lons, lats, timestamps, id_codes, t0, tf, output_file_path, step = 1, cols = NULL, icons = NULL, fixed_locs = NULL, fixed_locs_icons = NULL){
+create_trajectories_kml <- function(lons, lats, timestamps, id_codes, t0, tf, output_file_path, step = 1, cols = NULL, icons = NULL, fixed_locs = NULL, fixed_locs_icons = NULL, calls = NULL, calls_icons = NULL){
 
   #get number of individuals
   n_inds <- nrow(lons)
@@ -44,6 +46,20 @@ create_trajectories_kml <- function(lons, lats, timestamps, id_codes, t0, tf, ou
     icons <- rep('http://maps.google.com/mapfiles/kml/paddle/blu-blank-lv.png',n_inds)
   }
 
+  #if calls are specified, get number of call types
+  #if no calls icons specified, use diamonds for all call types
+  if(!is.null(calls)){
+    call_types <- unique(calls$call_type)
+    if(is.null(calls_icons)){
+      calls_icons <- rep('https://maps.google.com/mapfiles/kml/shapes/open-diamond.png', length(call_types))
+    }
+  }
+  #if calls are specified, create column for icons
+  if(!is.null(calls)){
+    calls$icon <- ''
+    calls$icon <- call_types[match(calls$call_type, call_types)]
+  }
+
   #get time indexes
   tidxs <- seq(t0,tf,step)
 
@@ -55,7 +71,7 @@ create_trajectories_kml <- function(lons, lats, timestamps, id_codes, t0, tf, ou
   cat("<kml xmlns=\"http://www.opengis.net/kml/2.2\" xmlns:gx=\"http://www.google.com/kml/ext/2.2\">\n")
   cat("<Document>\n")
 
-  #locations - perhaps we want to uncomment and add some in - not currently implemented
+  #plot fixed locations
   if(!is.null(fixed_locs)){
     if(is.null(fixed_locs_icons)){
       fixed_locs_icons <- rep('https://maps.google.com/mapfiles/kml/shapes/target.png', nrow(fixed_locs))
@@ -68,12 +84,19 @@ create_trajectories_kml <- function(lons, lats, timestamps, id_codes, t0, tf, ou
        cat("</Point>\n")
        cat(paste("<Icon><href>",fixed_locs_icons[i],"</href></Icon>"))
        cat("</Placemark>\n")
-   }
     }
+  }
 
   #tracks of ids
-  for (i in 1:n_inds) {
+  for(i in 1:n_inds){
     cat(paste("<Style id=\"track-",id_codes[i],"\"><IconStyle><scale>0.5</scale><Icon><href>",icons[i],"</href></Icon></IconStyle><LineStyle><color>",as.character(cols[i]),"</color><colorMode>normal</colorMode></LineStyle></Style>\n",sep=""))
+  }
+
+  #tracks of calls
+  if(!is.null(calls)){
+    for(i in 1:nrow(calls)){
+      cat(paste("<Style id=\"track-call",i,"\"><IconStyle><scale>0.5</scale><Icon><href>",calls$icon[i],"</href></Icon></IconStyle><LineStyle><color>",'ffffffff',"</color><colorMode>normal</colorMode></LineStyle></Style>\n",sep=""))
+    }
   }
 
   #time strings
@@ -81,8 +104,8 @@ create_trajectories_kml <- function(lons, lats, timestamps, id_codes, t0, tf, ou
   timestamps <- gsub(' ','T',timestamps)
   timestamps <- paste0(timestamps,'.000Z')
 
-  #locations
-  for (i in 1:n_inds) {
+  #locations of individuals
+  for(i in 1:n_inds){
     cat("<Folder>\n")
     cat(paste("<name>",id_codes[i],"</name>\n",sep=""))
     cat("<Placemark>")
@@ -92,21 +115,44 @@ create_trajectories_kml <- function(lons, lats, timestamps, id_codes, t0, tf, ou
     cat("<gx:Track>\n")
     cat("<gx:altitudeMode>relativeToGround</gx:altitudeMode>\n")
 
-    # FOR EACH TIME - location and any calls
-    for (tt in tidxs) {
+    # FOR EACH TIME - location of individuals
+    for(tt in tidxs){
       if(!is.na(lons[i,tt])){
         cat(sprintf("<when>%s</when>\n",timestamps[tt]),sep="")
       }
     }
     # FOR EACH TIME
-    for (tt in tidxs) {
+    for(tt in tidxs){
       if(!is.na(lons[i,tt])){
         cat(sprintf("<gx:coord>%s</gx:coord>\n",paste(lons[i,tt],lats[i,tt])),sep="")
       }
     }
+
     cat("</gx:Track>\n")
     cat("</Placemark>\n")
     cat("</Folder>\n")
+  }
+
+  #locations and times of calls
+  if(!is.null(calls)){
+    for(i in 1:nrow(calls)){
+      cat("<Folder>\n")
+      cat(paste("<name>",i,"</name>\n",sep=""))
+      cat("<Placemark>")
+      cat(paste("<styleUrl>#track-call",i,"</styleUrl>\n",sep=""))
+      cat("<visibility>0</visibility>\n")
+      cat(paste("<name>",i,"</name>\n",sep=''))
+      cat("<gx:Track>\n")
+      cat("<gx:altitudeMode>relativeToGround</gx:altitudeMode>\n")
+
+      # when and where
+      cat(sprintf("<when>%s</when>\n",timestamps[calls$time_idx[i]]),sep="")
+      cat(sprintf("<gx:coord>%s</gx:coord>\n",paste(lons[calls$ind_idx[i],calls$time_idx[i]],lats[calls$ind_idx[i],calls$time_idx[i]])),sep="")
+
+      cat("</gx:Track>\n")
+      cat("</Placemark>\n")
+      cat("</Folder>\n")
+    }
   }
 
   cat("</Document>\n")
---
+++ R/get_group_elongation.R
@@ -0,0 +1,54 @@
+#'Get group polarization
+#'
+#'Computes the elongation of the group at each time step `t` as well as the long axis vector.
+#'The elongation is a measure of how "line-link" vs "circle-like" the group is, ranging from 0 (completely circular) to 1 (completely elongated)
+#'The elongation is defined by first performing a PCA on the x/y data of the group. The axis of greatest variation is defined
+#'as the 'long axis' and the axis perpendicular to this is defined as the 'short axis'. The elonation is then
+#'defined as 1 - (length of short axis / length of long axis).
+#'
+#' @author Ariana Strandburg-Peshkin (primary author)
+#' @author NOT YET CODE REVIEWED
+#'
+#' @param xs `N x n_times` matrix giving x coordinates of each individual over time
+#' @param ys `N x n_times` matrix giving y coordinates of each individual over time
+#' @param min_inds_tracked if specified, sets a minimum number of individuals that must be tracked at any moment in time to compute elongation (otherwise the elongation will be NA at that time point). Default is 3 (below this, elongation is generally undefined)
+#'
+#' @returns Returns a list containing:
+#' `out$elongation`: a vector of length `n_times` giving the elongation of the group over time
+#' `out$long_axis_angle`: a vector of length `n_times` giving the angle of the long axis over time
+#' @export
+get_group_elongation <- function(xs, ys, min_inds_tracked = 3){
+
+  #get number of individuals
+  n_inds <- nrow(xs)
+  n_times <- ncol(xs)
+
+  #check matrix dimensions
+  if(nrow(ys) != n_inds || ncol(ys) != n_times){
+    stop('xs and ys matrices must have same dimensions')
+  }
+
+  #vectors to hold output
+  elongs <- long_axis_angles <- rep(NA, n_times)
+
+  for(t in 1:n_times){
+    x <- xs[,t]
+    y <- ys[,t]
+    non_na <- which(!is.na(x) & !is.na(y))
+    if(length(non_na) > min_inds_tracked){
+      pc <- prcomp(cbind(x[non_na],y[non_na]), center = T, scale = F)
+      long_axis <- pc$rotation[,1]
+      long_axis_angles[t] <- atan2(long_axis[1], long_axis[2])
+      elongs[t] <- 1 - (pc$sdev[2]/pc$sdev[1])
+    }
+
+  }
+
+  out <- list()
+  out$elongation <- elongs
+  out$long_axis_angle <- long_axis_angles
+
+  #output polarization over time
+  return(out)
+
+}
